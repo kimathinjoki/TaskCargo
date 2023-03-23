1c59df54b45c609a7a86335d1954b3f9ad6b7a2f
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-23 12:05:06 -0700

    create a navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Navbar.css"
 
 
 function Navbar(){
     return(
         <>
+            <nav>
+                 <a href="#">HOME</a>
+                <a href="#">TASKS</a>
+                <a href="#">ABOUT</a>
+                <div id="indicator"></div>
+            </nav>
         </>
     )
 }
