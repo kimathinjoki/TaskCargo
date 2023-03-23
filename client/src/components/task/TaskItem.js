6daca485dcf018782b7eca3c6789a9a567e21b65
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-23 12:04:51 -0700

    create a task card

--- client/src/components/task/TaskItem.js
+++ client/src/components/task/TaskItem.js
@@ -1,9 +1,29 @@
 import React from "react";
+import './TaskItem.css'
+import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
 
 
 function TaskItem(){
     return(
         <>
+        <div className="col-11" id="task_item">
+            <div className="card" id="task_card" >
+                <div className="card-body">
+                    <h5>Card title </h5>
+                    <div>
+                        <p id="description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+
+                    <div id="item_footer">
+                        <h6><span>Status:</span> <span>Created</span>  </h6>
+                        <h6 id="del_edit"><span><AiOutlineEdit/></span> <span><AiOutlineDelete/></span></h6>
+                    </div>
+                    
+                   
+                    {/* <a target={'_blank'} href="#" className="btn btn-primary">{'VISIT PAGE'}</a> */}
+                </div>
+            </div>
+        </div>    
         </>
     )
 }
