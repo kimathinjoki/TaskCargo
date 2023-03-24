eed64a0a18caa310d42f564835ca1d63e8b8ffb5
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-24 05:59:14 -0700

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-
 import Home from './components/home/Home';
+import Auth from './components/auth/Auth';
+import Landingpage from './components/home/Landingpage';
+ import {AiOutlineLogin} from "react-icons/ai"
+ import {Route, Routes, Link} from 'react-router-dom'
 
 function App() {
+
+
+function auth(){
+  <Link to="/auth"></Link>
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div >
+        <h3 >
+          <Link to="/" id="logo_title">TASK-CARGO</Link>  
+        </h3>
 
-        
-        <Home/>
+        <button id="auth_logo" >
+        <Link to="/auth"><AiOutlineLogin/></Link>
+        </button>
+
+      </div>
+      {/* <Home/> */}
+
+     
+
+
+
+
+      <Routes>
+        <Route path="/" element={<Landingpage/>}/>
+       <Route path="/auth" element={<Auth/>}/>
+       <Route path="/home" element={<Home/>}/>
+      </Routes>
+
       
-      </header>
+        
     </div>
   );
 }
