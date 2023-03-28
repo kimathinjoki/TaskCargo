ddf4e650f4352de31d4a053f453c614617feeff7
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-28 12:22:37 -0700

    add cors

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -6,20 +6,20 @@ class ApplicationController < ActionController::API
 
 
     # Enable CORS Headers
-  configure do
-    enable :cross_origin
-  end
+#   configure do
+#     enable :cross_origin
+#   end
 
-  before do
-    response.headers['Access-Control-Allow-Origin'] = '*'
-  end
+#   before do
+#     response.headers['Access-Control-Allow-Origin'] = '*'
+#   end
 
-  options "*" do
-    response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    200
-  end
+#   options "*" do
+#     response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE, OPTIONS"
+#     response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+#     response.headers["Access-Control-Allow-Origin"] = "*"
+#     200
+#   end
 
     # has data into web token
 
